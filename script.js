2c8d5389abdc12cfa146550e9dec797a1d629c91
Matthew Griffin <matti@DESKTOP-FU78GDQ.(none)>
2024-12-23 12:59:58 +0000

    Added intro floating boxes

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-window.addEventListener('scroll', function() { 
-    const movingDiv = document.getElementById('movingDiv'); 
-    const scrollPosition = window.scrollY; // Get vertical scroll position 
+// window.addEventListener('scroll', function() { 
+    // const movingDiv = document.getElementById('movingDiv'); 
+    // const scrollPosition = window.scrollY; // Get vertical scroll position 
  
-    // Move the div based on scroll position 
-    movingDiv.style.transform = `translateY(${scrollPosition}vh)`; 
-}); 
+    // // Move the div based on scroll position 
+    // movingDiv.style.transform = `translateY(calc(${scrollPosition}*2)px)`; 
+	
+// });
+
+// var targetNode = document.getElementById('stretchId');
+// var observer = new MutationObserver(function(){
+    // if(targetNode.style.display != 'none'){
+        // width = 50vw
+    // }
+// });
+// observer.observe(targetNode, { attributes: true, childList: true }); 
+
+// $('elements').onScreen({
+   // container: window,
+   // direction: 'vertical',
+   // doIn: function() {
+     // // Do something to the matched elements as they come in
+   // },
+   // doOut: function() {
+     // // Do something to the matched elements as they get off scren
+   // },
+   // tolerance: 0,
+   // throttle: 50,
+   // toggleClass: 'onScreen',
+   // lazyAttr: null,
+   // lazyPlaceholder: 'someImage.jpg',
+   // debug: false
+// });
